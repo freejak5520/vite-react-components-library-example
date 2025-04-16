419fca909998dbaa7597fd018a63045e47cbf36d
Hyunjin Lee <hyunjin@veluga.kr>
2025-04-16 19:26:16 +0900

    컴포넌트 라이브러리 프로젝트 초기 설정
    Vite, React, TypeScript 통합 및 ESLint 구성 추가. 기본 파일 구조 생성, .gitignore 및 tsconfig 파일 포함. README.md에 사용법 설명 추가.

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import react from "@vitejs/plugin-react-swc";
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    dts({ include: ["lib"], tsconfigPath: "./tsconfig.build.json" }),
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "lib/main.ts"),
+      formats: ["es"],
+    },
+    copyPublicDir: false,
+    rollupOptions: {
+      external: ["react", "react/jsx-runtime"],
+    },
+  },
+});
