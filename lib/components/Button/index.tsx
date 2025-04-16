23ce2c8c3a1de49ecd4cdfb524f5a2e64a910304
Hyunjin Lee <hyunjin@veluga.kr>
2025-04-16 19:55:20 +0900

    CSS 모듈 적용
    Squashed commit of the following:
    
    commit a18d4239bc65c45cc154f9612f4ded8c809aead2
    Author: Hyunjin Lee <hyunjin@veluga.kr>
    Date:   Wed Apr 16 19:55:07 2025 +0900
    
        컴포넌트 export 방식 변경
    
    commit 41f4fc510408b909fe7c826f58675440487acef0
    Author: Hyunjin Lee <hyunjin@veluga.kr>
    Date:   Wed Apr 16 19:50:00 2025 +0900
    
        glob 적용
    
    commit 6eb27d19a0e618246219ccf4ef6111c99dcf9351
    Author: Hyunjin Lee <hyunjin@veluga.kr>
    Date:   Wed Apr 16 19:42:37 2025 +0900
    
        vite-plugin-lib-inject-css 적용
    
    commit 5f03f7db2adacd7b91667257067e288a0b86dcde
    Merge: e279389 f4fc260
    Author: Hyunjin Lee <hyunjin@veluga.kr>
    Date:   Wed Apr 16 19:37:28 2025 +0900
    
        Merge branch 'main' into chore/css
    
    commit e2793896aad4afea54b55c04c81599c79fcb4992
    Author: Hyunjin Lee <hyunjin@veluga.kr>
    Date:   Wed Apr 16 19:37:00 2025 +0900
    
        단순 module css 적용

--- lib/components/Button/index.tsx
+++ lib/components/Button/index.tsx
@@ -1,3 +1,7 @@
-export default function Button() {
-  return <button>Click me</button>;
+import { PropsWithChildren } from "react";
+
+import styles from "./styles.module.css";
+
+export function Button({ children }: PropsWithChildren) {
+  return <button className={styles.button}>{children}</button>;
 }
--- lib/main.ts
+++ lib/main.ts
@@ -1,1 +1,1 @@
-export * as Button from "./components/Button";
+export { Button } from "./components/Button";
